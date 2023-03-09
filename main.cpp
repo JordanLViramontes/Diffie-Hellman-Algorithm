577ef08188635c80887177b79afbdffb7f32ccab
Jordan Viramontes <jordan.viramontes@gmail.com>
2023-03-08 17:21:07 -0800

    added thesis

--- main.cpp
+++ main.cpp
@@ -1,3 +1,10 @@
+// Diffie-Hellman-Algorithm
+//
+// Purpose is to implement the Diffie-Hellman Algorithm of encryption in C++ 
+// for my group's final project in CS111
+//
+// steps taken from: https://www.javatpoint.com/diffie-hellman-algorithm-in-java
+
 int main() {
 
     return 0;
